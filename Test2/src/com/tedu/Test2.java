ce4eebf6aea1b0b23ff96df599ce5860670648b4
Administrator <Administrator@USER-20161013UJ>
2019-05-30 14:58:33 +0800

    222

--- Test2/src/com/tedu/Test2.java
+++ Test2/src/com/tedu/Test2.java
@@ -2,6 +2,9 @@ package com.tedu;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		for (int i = 0;; i++) {
+			System.out.println("hello");
+		}
+		
 	}
 }
